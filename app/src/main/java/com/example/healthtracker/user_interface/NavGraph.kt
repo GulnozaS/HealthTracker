b44e01725d0c0e7f157b1a87347f9845a1ce790d
220278cs03 <220278@akfauniversity.org>
2025-04-15 10:19:51 +0500

    home page added

--- app/src/main/java/com/example/healthtracker/user_interface/NavGraph.kt
+++ app/src/main/java/com/example/healthtracker/user_interface/NavGraph.kt
@@ -7,6 +7,7 @@ import androidx.navigation.compose.rememberNavController
 import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import com.example.healthtracker.user_interface.screens.AccountSetupScreen
+import com.example.healthtracker.user_interface.screens.HomeScreen
 import com.example.healthtracker.user_interface.screens.LoginScreen
 import com.example.healthtracker.user_interface.screens.SignUpScreen
 
@@ -14,9 +15,10 @@ import com.example.healthtracker.user_interface.screens.SignUpScreen
 fun NavGraph() {
     val navController = rememberNavController()
 
-    NavHost(navController = navController, startDestination = "account_setup") {
+    NavHost(navController = navController, startDestination = "home") {
         composable("signup") { SignUpScreen(navController) }
         composable("login") { LoginScreen(navController) }
         composable("account_setup") { AccountSetupScreen(navController) }
+        composable("home") { HomeScreen(navController) }
     }
 }
